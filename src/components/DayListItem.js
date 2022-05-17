b5ce2eca2e3e698c35de71f4cdfdbed5740f5483
Ben Finlay <benjamin.finlay@gmail.com>
2022-05-17 15:17:43 -0400

    Add DayListItem js, scss, and tests for the individual days in our scheduler.

---
+++ src/components/DayListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+const formatSpots = (spots) => {
+  if (spots === 0) {
+    return "no spots remaining"
+  } else if (spots === 1) {
+    return "1 spot remaining"
+  } else {
+    return (`${spots} spots remaining`)
+  }
+}
+
+
+export default function DayListItem(props) {
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots,
+  });
+
+  return (
+    <li className={dayClass}
+        onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
