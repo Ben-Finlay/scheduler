e5be81dc2cf28eec9fca506884e18dd47209db06
Ben Finlay <benjamin.finlay@gmail.com>
2022-05-17 15:59:12 -0400

    Add DayList to render DayListItem, and added useState to Application.js so all components can access day.

---
+++ src/components/DayList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  return (
+    <ul>
+      {props.days.map((d) => (
+        <DayListItem
+          key={d.id}
+          name={d.name}
+          spots={d.spots}
+          selected={d.name === props.day}
+          setDay={() => props.setDay(d.name)}
+        />
+      ))}
+    </ul>
+  );
+}
+
+
+
+
