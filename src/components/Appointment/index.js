88f984c4e1c19d4b07854132818a885d53665c76
Ben Finlay <benjamin.finlay@gmail.com>
2022-05-18 14:56:12 -0400

    Add Header and Empty components for the Appointment section of the site.

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'components/Appointment/styles.scss';
+
+import Header from './Header'
+
+export default function Appointment(props) {
+
+
+  return (
+    <article className="appointment"></article>
+  )
+
+}
