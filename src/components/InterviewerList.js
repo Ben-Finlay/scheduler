659a14b492da48c647da74036fc5eeade33d1942
Ben Finlay <benjamin.finlay@gmail.com>
2022-05-18 13:15:07 -0400

    Add commit for functionality for InterviewerList which will render InterviewerListItems

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import 'components/InterviewerList.scss';
+import InterviewerListItem from './InterviewerListItem';
+
+
+export default function InterviewerList(props) {
+  
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+      {props.interviewers.map((p) => (
+        <InterviewerListItem
+          key={p.id}
+          id={p.id}
+          name={p.name}
+          avatar={p.avatar}
+          selected={p.id === props.interviewer}
+          setInterviewer={props.setInterviewer}
+
+        />
+      ))}
+
+
+
+      </ul>
+    </section>
+
+  )
+}
+
+
+
+/*
+export default function DayList(props) {
+  return (
+    <ul>
+      {props.days.map((d) => (
+        <DayListItem
+          key={d.id}
+          name={d.name}
+          spots={d.spots}
+          selected={d.name === props.day}
+          setDay={() => props.setDay(d.name)}
+        />
+      ))}
+    </ul>
+  );
+}
+*/
